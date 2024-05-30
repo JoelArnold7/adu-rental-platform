709a00a9b115c67a74a0001cd69878031e1bd450
JoelArnold7 <joelsarnold96@gmail.com>
2024-05-30 16:26:18 -0700

    Set up environment variables and deploy

--- adu-admin/server.js
+++ adu-admin/server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/adu', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
